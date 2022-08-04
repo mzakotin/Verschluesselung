a9836d4a2235d55fae50324556423f42aede4d23
mzakotin <70580871+mzakotin@users.noreply.github.com>
2022-08-04 10:19:31 +0200

    Add files via upload

---
+++ Verschluesselung/Verschluesselung/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Verschluesselung
+{
+    partial class fm_Verschluesselung
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_Eingabe = new System.Windows.Forms.TextBox();
+            this.CB_Caesar = new System.Windows.Forms.ComboBox();
+            this.TB_Ausgabe = new System.Windows.Forms.TextBox();
+            this.GB_Chiffre = new System.Windows.Forms.GroupBox();
+            this.RB_Vigenere = new System.Windows.Forms.RadioButton();
+            this.RB_Caesar = new System.Windows.Forms.RadioButton();
+            this.TB_Schluessel = new System.Windows.Forms.TextBox();
+            this.Bt_Verschluesseln = new System.Windows.Forms.Button();
+            this.Bt_einlesen = new System.Windows.Forms.Button();
+            this.OFD_Chiffre = new System.Windows.Forms.OpenFileDialog();
+            this.GB_Chiffre.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TB_Eingabe
+            // 
+            this.TB_Eingabe.Location = new System.Drawing.Point(58, 41);
+            this.TB_Eingabe.Name = "TB_Eingabe";
+            this.TB_Eingabe.Size = new System.Drawing.Size(100, 20);
+            this.TB_Eingabe.TabIndex = 0;
+            this.TB_Eingabe.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_Eingabe_KeyPress);
+            // 
+            // CB_Caesar
+            // 
+            this.CB_Caesar.FormattingEnabled = true;
+            this.CB_Caesar.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25"});
+            this.CB_Caesar.Location = new System.Drawing.Point(332, 40);
+            this.CB_Caesar.Name = "CB_Caesar";
+            this.CB_Caesar.Size = new System.Drawing.Size(51, 21);
+            this.CB_Caesar.TabIndex = 1;
+            this.CB_Caesar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CB_Caesar_KeyPress);
+            // 
+            // TB_Ausgabe
+            // 
+            this.TB_Ausgabe.Location = new System.Drawing.Point(58, 202);
+            this.TB_Ausgabe.Name = "TB_Ausgabe";
+            this.TB_Ausgabe.ReadOnly = true;
+            this.TB_Ausgabe.Size = new System.Drawing.Size(100, 20);
+            this.TB_Ausgabe.TabIndex = 2;
+            this.TB_Ausgabe.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_Ausgabe_KeyPress);
+            // 
+            // GB_Chiffre
+            // 
+            this.GB_Chiffre.Controls.Add(this.RB_Vigenere);
+            this.GB_Chiffre.Controls.Add(this.RB_Caesar);
+            this.GB_Chiffre.Location = new System.Drawing.Point(483, 40);
+            this.GB_Chiffre.Name = "GB_Chiffre";
+            this.GB_Chiffre.Size = new System.Drawing.Size(115, 89);
+            this.GB_Chiffre.TabIndex = 3;
+            this.GB_Chiffre.TabStop = false;
+            this.GB_Chiffre.Text = "Art der Chiffre";
+            // 
+            // RB_Vigenere
+            // 
+            this.RB_Vigenere.AutoSize = true;
+            this.RB_Vigenere.Location = new System.Drawing.Point(6, 53);
+            this.RB_Vigenere.Name = "RB_Vigenere";
+            this.RB_Vigenere.Size = new System.Drawing.Size(67, 17);
+            this.RB_Vigenere.TabIndex = 1;
+            this.RB_Vigenere.TabStop = true;
+            this.RB_Vigenere.Text = "Vigenère";
+            this.RB_Vigenere.UseVisualStyleBackColor = true;
+            this.RB_Vigenere.CheckedChanged += new System.EventHandler(this.RB_Vigenere_CheckedChanged);
+            // 
+            // RB_Caesar
+            // 
+            this.RB_Caesar.AutoSize = true;
+            this.RB_Caesar.Location = new System.Drawing.Point(6, 30);
+            this.RB_Caesar.Name = "RB_Caesar";
+            this.RB_Caesar.Size = new System.Drawing.Size(58, 17);
+            this.RB_Caesar.TabIndex = 0;
+            this.RB_Caesar.TabStop = true;
+            this.RB_Caesar.Text = "Caesar";
+            this.RB_Caesar.UseVisualStyleBackColor = true;
+            this.RB_Caesar.CheckedChanged += new System.EventHandler(this.RB_Caesar_CheckedChanged);
+            // 
+            // TB_Schluessel
+            // 
+            this.TB_Schluessel.Location = new System.Drawing.Point(58, 109);
+            this.TB_Schluessel.Name = "TB_Schluessel";
+            this.TB_Schluessel.Size = new System.Drawing.Size(100, 20);
+            this.TB_Schluessel.TabIndex = 4;
+            this.TB_Schluessel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_Schluessel_KeyPress);
+            // 
+            // Bt_Verschluesseln
+            // 
+            this.Bt_Verschluesseln.Location = new System.Drawing.Point(319, 106);
+            this.Bt_Verschluesseln.Name = "Bt_Verschluesseln";
+            this.Bt_Verschluesseln.Size = new System.Drawing.Size(106, 23);
+            this.Bt_Verschluesseln.TabIndex = 5;
+            this.Bt_Verschluesseln.Text = "Verschlüsseln";
+            this.Bt_Verschluesseln.UseVisualStyleBackColor = true;
+            this.Bt_Verschluesseln.Click += new System.EventHandler(this.Bt_Verschluesseln_Click);
+            // 
+            // Bt_einlesen
+            // 
+            this.Bt_einlesen.Location = new System.Drawing.Point(319, 147);
+            this.Bt_einlesen.Name = "Bt_einlesen";
+            this.Bt_einlesen.Size = new System.Drawing.Size(106, 23);
+            this.Bt_einlesen.TabIndex = 6;
+            this.Bt_einlesen.Text = "Datei einlesen";
+            this.Bt_einlesen.UseVisualStyleBackColor = true;
+            this.Bt_einlesen.Click += new System.EventHandler(this.Bt_einlesen_Click);
+            // 
+            // OFD_Chiffre
+            // 
+            this.OFD_Chiffre.DefaultExt = "*.txt";
+            this.OFD_Chiffre.FileName = "C:\\Users\\user\\Documents\\BFW\\Praktikum_2020\\Projekt2020_MZakotin\\Verschluesselung";
+            this.OFD_Chiffre.Filter = "Textdateien (*.txt)|*.txt";
+            // 
+            // fm_Verschluesselung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Bt_einlesen);
+            this.Controls.Add(this.Bt_Verschluesseln);
+            this.Controls.Add(this.TB_Schluessel);
+            this.Controls.Add(this.GB_Chiffre);
+            this.Controls.Add(this.TB_Ausgabe);
+            this.Controls.Add(this.CB_Caesar);
+            this.Controls.Add(this.TB_Eingabe);
+            this.Name = "fm_Verschluesselung";
+            this.Text = "Verschlüsselung";
+            this.Load += new System.EventHandler(this.Fm_Caesar_Load);
+            this.GB_Chiffre.ResumeLayout(false);
+            this.GB_Chiffre.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_Eingabe;
+        private System.Windows.Forms.ComboBox CB_Caesar;
+        private System.Windows.Forms.TextBox TB_Ausgabe;
+        private System.Windows.Forms.GroupBox GB_Chiffre;
+        private System.Windows.Forms.RadioButton RB_Vigenere;
+        private System.Windows.Forms.RadioButton RB_Caesar;
+        private System.Windows.Forms.TextBox TB_Schluessel;
+        private System.Windows.Forms.Button Bt_Verschluesseln;
+        private System.Windows.Forms.Button Bt_einlesen;
+        private System.Windows.Forms.OpenFileDialog OFD_Chiffre;
+    }
+}
+
---
+++ Verschluesselung/Verschluesselung/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+
+namespace Verschluesselung
+{
+    public partial class fm_Verschluesselung : Form
+    {
+        public fm_Verschluesselung()
+        {
+            InitializeComponent();
+        }
+
+        private void Fm_Caesar_Load(object sender, EventArgs e)
+        {
+            CB_Caesar.Text = "0";
+            TB_Ausgabe.ReadOnly = true;
+            RB_Caesar.Checked = false;
+            RB_Vigenere.Checked = false;
+        }
+
+        private void TB_Eingabe_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int keycode = e.KeyChar;
+            char caesar;
+            int zwischen;
+
+            //8: Rücktaste (Ascii-Code)
+            if (!((keycode >= 'A' && keycode <= 'Z') || (keycode >= 'a' && keycode <= 122) || keycode == 8))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+
+                if (RB_Caesar.Checked == true)
+                {
+                    if (CB_Caesar.Text != "" && keycode != 8)
+                    {
+
+                        zwischen = keycode + CB_Caesar.SelectedIndex;
+
+                        //check Großbuchstabe-kleinbuchstabe
+                        if (keycode >= 'A' && keycode <= 'Z')
+                        {
+                            if (zwischen > 'Z')
+                            {
+                                caesar = (char)(zwischen - 26);
+                            }
+                            else
+                            {
+                                caesar = (char)(zwischen);
+                            }
+
+                        }
+                        else
+                        {
+                            if (zwischen > 'z')
+                            {
+                                caesar = (char)(zwischen - 26);
+                            }
+                            else
+                            {
+                                caesar = (char)(zwischen);
+                            }
+                        }
+                        TB_Ausgabe.Text += caesar.ToString();
+                    }
+
+                    else if (keycode == 8) { TB_Ausgabe.Clear(); }
+                }
+
+
+            }
+        }
+
+        private void CB_Caesar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int keycode = e.KeyChar;
+
+            //8: Rücktaste nicht erfüllt
+            if ((!(keycode >= '0' && keycode <= '9' || keycode == 8)) ||
+                (CB_Caesar.Text.Length > 1 && keycode != 8) || //maximal 2 Stellen
+                (CB_Caesar.Text == "0" && (keycode != 8)) || // wenn 0 eingetragen, nur löschen mit Rücktaste möglich
+                (CB_Caesar.Text == "2" && (!(keycode == 8 || keycode >= '0' && keycode <= '5'))) || //Zahlen von 20 bis 25
+                (!(CB_Caesar.Text == "" || CB_Caesar.Text == "1" || CB_Caesar.Text == "2") && (keycode != 8))) //keine weitere Stelle nach den Ziffern 3 bis 9
+
+            {
+                e.Handled = true;
+            }
+            else e.Handled = false;
+        }
+
+        private void RB_Caesar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RB_Caesar.Checked == true)
+            {
+                RB_Vigenere.Checked = false;
+                CB_Caesar.Visible = true;
+                TB_Schluessel.Hide();
+                Bt_Verschluesseln.Hide();
+                TB_Eingabe.Clear();
+                TB_Schluessel.Clear();
+                TB_Ausgabe.Clear();
+            }
+        }
+
+        private void RB_Vigenere_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RB_Vigenere.Checked == true)
+            {
+                RB_Caesar.Checked = false;
+                CB_Caesar.Hide();
+                TB_Schluessel.Visible = true;
+                Bt_Verschluesseln.Visible = true;
+                TB_Eingabe.Clear();
+                CB_Caesar.Text = "0";
+                TB_Schluessel.Clear();
+                TB_Ausgabe.Clear();
+            }
+        }
+
+        private void Bt_Verschluesseln_Click(object sender, EventArgs e)
+        {
+            if (RB_Vigenere.Checked == false) { MessageBox.Show("Es ist nichts ausgewählt worden!"); }
+
+            else
+            if (TB_Eingabe.Text == "" || TB_Schluessel.Text == "") { MessageBox.Show("Bitte Klartext und Schlüssel eingeben!"); }
+
+            else
+            {
+                string klartext = TB_Eingabe.Text;
+                string key = TB_Schluessel.Text;
+                char vigenere;
+                int i = 0;
+
+                if (klartext.Length != key.Length)
+                {
+                    MessageBox.Show("Klartext und Schlüssel müssen die gleiche Anzahl von Buchstaben haben!");
+                }
+
+                else
+
+                    foreach (char c in klartext)
+                    {
+                        int zwischen = c + key[i];
+
+                        //check Großbuchstabe-kleinbuchstabe
+                        if ((c >= 'A' && c <= 'Z') && (key[i] >= 'A' && key[i] <= 'Z'))
+                        {
+                            zwischen = zwischen - 65;
+
+                            if (zwischen > 'Z')
+                            {
+                                vigenere = (char)(zwischen - 26);
+                                TB_Ausgabe.Text += vigenere.ToString();
+                                i++;
+                            }
+                            else
+                            {
+                                vigenere = (char)(zwischen);
+                                TB_Ausgabe.Text += vigenere.ToString();
+                                i++;
+                            }
+
+                        }
+                        else
+                        {
+                            zwischen = zwischen - 97;
+
+                            if (zwischen > 'z')
+                            {
+                                vigenere = (char)(zwischen - 26);
+                                TB_Ausgabe.Text += vigenere.ToString();
+                                i++;
+                            }
+                            else
+                            {
+                                vigenere = (char)(zwischen);
+                                TB_Ausgabe.Text += vigenere.ToString();
+                                i++;
+                            }
+                        }
+                    }
+            }
+        }
+
+        private void TB_Schluessel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int keycode = e.KeyChar;                                        //8 = Rücktaste
+            if (!((keycode >= 'A' && keycode <= 'Z') || (keycode >= 'a' && keycode <= 'z') || keycode == 8))
+            {
+                e.Handled = true;
+            }
+            else e.Handled = false;
+
+            if (keycode == 8) { TB_Ausgabe.Clear(); }
+        }
+
+        private void TB_Ausgabe_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int keycode = e.KeyChar;
+            if (keycode == 8 && TB_Ausgabe.Text != "") { TB_Ausgabe.ReadOnly = false; }
+            else
+            { TB_Ausgabe.ReadOnly = true; e.Handled = true; }
+        }
+
+        private void Bt_einlesen_Click(object sender, EventArgs e)
+        {
+            if (OFD_Chiffre.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = OFD_Chiffre.FileName;
+                    /*using (Stream str = OFD_Chiffre.OpenFile())
+                    {
+                        Process.Start("notepad.exe", filePath);
+
+                    }*/
+
+                    //Read the contents of the file into a stream
+                    var fileStream = OFD_Chiffre.OpenFile();
+                    var fileContent = "Gedicht.txt";
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        TB_Eingabe.Text = fileContent;
+                    }
+
+                    if (RB_Caesar.Checked == false && RB_Vigenere.Checked == false) 
+                    { MessageBox.Show("Bitte wählen Sie die Art der Verschlüsselung aus!"); }
+
+                    else
+
+                        foreach (char c in fileContent)
+                        {
+                            if (CB_Caesar.Text != "" && TB_Eingabe.Text != "")
+                            {
+                                int zwischen = c + CB_Caesar.SelectedIndex;
+                                char caesar;
+
+                                //check Großbuchstabe-kleinbuchstabe
+                                if (c >= 'A' && c <= 'Z')
+                                {
+                                    if (zwischen > 'Z')
+                                    {
+                                        caesar = (char)(zwischen - 26);
+                                    }
+                                    else
+                                    {
+                                        caesar = (char)(zwischen);
+                                    }
+
+                                }
+                                else
+                                {
+
+                                    if (zwischen > 'z')
+                                    {
+                                        caesar = (char)(zwischen - 26);
+                                    }
+                                    else
+                                    {
+                                        caesar = (char)(zwischen);
+                                    }
+                                }
+
+                                if (c == ' ' || c == '.' || c == '\r')
+                                {
+                                    zwischen = c;
+                                    caesar = (char)(zwischen);
+                                }
+                                TB_Ausgabe.Text += caesar.ToString();
+                            }
+                        }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
